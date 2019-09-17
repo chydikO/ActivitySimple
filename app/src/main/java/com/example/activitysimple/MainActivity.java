2897e11ce33727d57ddd1e4b3067a7d2a7e8d0b6
chydikO <oleg.chudnovskiy@gmail.com>
2019-09-17 16:08:56 +0300

    HW Less_4

--- app/src/main/java/com/example/activitysimple/MainActivity.java
+++ app/src/main/java/com/example/activitysimple/MainActivity.java
@@ -2,7 +2,13 @@ package com.example.activitysimple;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 /**
  * Ссылки из урока:
@@ -22,9 +28,52 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnOk;
+    Button btnCancel;
+    EditText editTextLogin;
+    EditText getEditTextPass;
+    TextView resultText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnOk = findViewById(R.id.btnOk);
+        btnCancel = findViewById(R.id.btnCancel);
+        editTextLogin = findViewById(R.id.textViewLogin);
+        getEditTextPass = findViewById(R.id.textViewPass);
+        resultText = findViewById(R.id.textViewResult);
+
+        btnOk.setOnClickListener(clickListener);
+        btnCancel.setOnClickListener(clickListener);
+    }
+
+    private View.OnClickListener clickListener = new View.OnClickListener() {
+        public void onClick(View v) {
+            // do something when the button is clicked
+            if(v.getId() == R.id.btnOk) {
+                btnOkClick();
+            } else if(v.getId() == R.id.btnCancel) {
+                btnCancelClick();
+            }
+        }
+    };
+
+    private void btnOkClick() {
+        resultText.setText(getEditTextPass.getText());
+        changeBgColor(Color.BLUE);
+    }
+
+    private void btnCancelClick() {
+        editTextLogin.setText("");
+        getEditTextPass.setText("");
+        resultText.setText("");
+        changeBgColor(Color.WHITE);
+    }
+
+    private void changeBgColor(Integer color) {
+        View mainView = findViewById(R.id.container);
+        mainView.setBackgroundColor(color);
     }
 }
